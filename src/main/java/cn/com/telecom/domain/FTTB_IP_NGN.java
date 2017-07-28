c9b140dcc8f0d7462c9b0f36c3780060c5e95be6
Administrator <Administrator@ZIO1Q74HTICB2R9>
2017-07-28 11:08:30 +0800

    FTTB_IP_NGN

---
+++ src/main/java/cn/com/telecom/domain/FTTB_IP_NGN.java
@@ -0,0 +1,100 @@
+package cn.com.telecom.domain;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "FTTB_IP_NGN")
+public class FTTB_IP_NGN extends BaseEntity implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8816423668644300304L;
+	/**
+	 * 
+	 */
+	@Column(name="区域")
+	private String Area;
+	
+	@Column(name="CN2地址段")
+	private String CN2Adress;
+	
+	@Column(name="网关")
+	private String Gateway;
+	
+	@Column(name = "掩码")
+	private String Mask;
+	
+	@Column(name = "语音VLAN_ID")
+	private String VoiceVLAN_ID;
+	
+	@Column(name = "OLT")
+	private String OLT;
+	
+	@Column(name = "分配时间")
+	private String AssignTime;
+
+	public String getArea() {
+		return Area;
+	}
+
+	public void setArea(String area) {
+		Area = area;
+	}
+
+	public String getCN2Adress() {
+		return CN2Adress;
+	}
+
+	public void setCN2Adress(String cN2Adress) {
+		CN2Adress = cN2Adress;
+	}
+
+	public String getGateway() {
+		return Gateway;
+	}
+
+	public void setGateway(String gateway) {
+		Gateway = gateway;
+	}
+
+	public String getMask() {
+		return Mask;
+	}
+
+	public void setMask(String mask) {
+		Mask = mask;
+	}
+
+	public String getVoiceVLAN_ID() {
+		return VoiceVLAN_ID;
+	}
+
+	public void setVoiceVLAN_ID(String voiceVLAN_ID) {
+		VoiceVLAN_ID = voiceVLAN_ID;
+	}
+
+	public String getOLT() {
+		return OLT;
+	}
+
+	public void setOLT(String oLT) {
+		OLT = oLT;
+	}
+
+	public String getAssignTime() {
+		return AssignTime;
+	}
+
+	public void setAssignTime(String assignTime) {
+		AssignTime = assignTime;
+	}
+	
+	
+
+}
