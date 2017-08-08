19486b8ace1d4f4fb5fa8c558bc4a43fcb054f7d
c986022387 <986022387@qq.com>
2017-08-08 15:50:49 +0800

    多动态查询模板

--- src/main/java/cn/com/telecom/repository/InformationOfTerminalRepository.java
+++ src/main/java/cn/com/telecom/repository/InformationOfTerminalRepository.java
@@ -4,13 +4,14 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import cn.com.telecom.domain.InformationOfTerminal;
 
 @Repository
-public interface InformationOfTerminalRepository extends CrudRepository<InformationOfTerminal, Integer>{
+public interface InformationOfTerminalRepository extends CrudRepository<InformationOfTerminal, Integer>, JpaSpecificationExecutor<InformationOfTerminal>{
 	
 	List<InformationOfTerminal> findAll();
 	
--- src/main/java/cn/com/telecom/service/InformationOfTerminalService.java
+++ src/main/java/cn/com/telecom/service/InformationOfTerminalService.java
@@ -2,7 +2,15 @@ package cn.com.telecom.service;
 
 import java.util.List;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import cn.com.telecom.domain.InformationOfTerminal;
@@ -33,10 +41,43 @@ public class InformationOfTerminalService {
 		Items<InformationOfTerminal> items = getItems(ascOrDesc, pageIndex, pageSize);
 		return items;
 	}
+	
+	/**
+	 * 多动态查询
+	 * @param ascOrDesc
+	 * @param pageIndex
+	 * @param pageSize
+	 * @return
+	 */
+	public Items<InformationOfTerminal> findByMultiple(String ascOrDesc, Integer pageIndex, Integer pageSize){
+		Items<InformationOfTerminal> items = getItemsByMultiple(ascOrDesc, pageIndex, pageSize);
+		return items;
+	}
 
 	
 	/**
-	 * 封装Items
+	 * 动态生成where语句查询并分装items
+	 * @param ascOrDesc
+	 * @param pageIndex
+	 * @param pageSize
+	 * @return
+	 */
+	private Items<InformationOfTerminal> getItemsByMultiple(String ascOrDesc, Integer pageIndex, Integer pageSize) {
+		PageRequest pageRequest = Utils.getPageRequest(ascOrDesc, "id", pageIndex, pageSize);
+		//动态生成Where语句
+		Specification<InformationOfTerminal> specification = getWhereClause();
+		Page<InformationOfTerminal> page = this.informationOfTerminalRepository.findAll(specification,pageRequest);
+		
+		Items<InformationOfTerminal> items = new Items<InformationOfTerminal>();
+		items.setPageIndex(pageIndex);
+		items.setPageSize(pageSize);
+		items.setT(page);
+		return items;
+	}
+
+	
+	/**
+	 * 查询全部并封装Items
 	 * @param ascOrDesc
 	 * @param pageIndex
 	 * @param pageSize
@@ -48,6 +89,21 @@ public class InformationOfTerminalService {
 		items.setPageSize(pageSize);
 		items.setT(informationOfTerminalRepository.findAll(Utils.getPageRequest(ascOrDesc, "id", pageIndex, pageSize)));
 		return items;
+	}
+	
+	/**
+	 * 动态生成where语句
+	 * @return
+	 */
+	private Specification<InformationOfTerminal> getWhereClause(){
+		return new Specification<InformationOfTerminal>() {
+
+			public Predicate toPredicate(Root<InformationOfTerminal> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+		};
 	}
 	
 }
